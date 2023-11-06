c35925c7e9d53687a871569fd9c1ca202c3554c7
Hecroesmo <hecroesmo.jesus77@outlook.com>
2023-11-06 10:40:13 +0100

    tratamento de cors para porta local 3000

--- src/main/java/com/example/testemarcos/config/SegurancaConfig.java
+++ src/main/java/com/example/testemarcos/config/SegurancaConfig.java
@@ -1,6 +1,5 @@
 package com.example.testemarcos.config;
 
-import com.example.testemarcos.repository.ContaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +17,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.testemarcos.repository.ContaRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -76,16 +75,16 @@ public class SegurancaConfig {
         return configuration.getAuthenticationManager();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-            }
-        };
-    }
+    // @Bean
+    // public WebMvcConfigurer corsConfigurer() {
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**")
+    //                     .allowedOrigins("*")
+    //                     .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    //         }
+    //     };
+    // }
 
 }
